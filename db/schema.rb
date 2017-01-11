2adca1253dc9100e65dd816125cf8a88f51fe7a0
Jackrim1 <jriminton@gmail.com>
2017-01-11 20:49:16 +0000

    List ID added to item, the correct way around

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111160151) do
+ActiveRecord::Schema.define(version: 20170111204839) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
