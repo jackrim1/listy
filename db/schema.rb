738d5cac80cf142084fb1713929c55ddffcf3346
Jackrim1 <jriminton@gmail.com>
2017-01-11 16:00:53 +0000

    Added Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111155938) do
+ActiveRecord::Schema.define(version: 20170111160034) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
